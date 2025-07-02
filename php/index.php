9ef973c77574fff08469acee84d4e952f7bf61ce
hmagh1 <129678389+hmagh1@users.noreply.github.com>
2025-07-02 11:13:09 +0100

    Update index.php

--- php/index.php
+++ php/index.php
@@ -1,37 +1,27 @@
 <?php
 ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 header("Content-Type: application/json");
 
-$request = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
+$requestUri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
 $method = $_SERVER['REQUEST_METHOD'];
 
-switch (true) {
-    case $method === 'POST' && preg_match('#^/users/?$#', $request):
-        require 'users/create.php';
-        break;
-
-    case $method === 'GET' && preg_match('#^/users/?$#', $request):
-        require 'users/read.php';
-        break;
-
-    case $method === 'GET' && preg_match('#^/users/(\d+)$#', $request, $matches):
-        $_GET['id'] = $matches[1];
-        require 'users/read_one.php';
-        break;
-
-    case $method === 'PUT' && preg_match('#^/users/(\d+)$#', $request, $matches):
-        $_GET['id'] = $matches[1];
-        require 'users/update.php';
-        break;
-
-    case $method === 'DELETE' && preg_match('#^/users/(\d+)$#', $request, $matches):
-        $_GET['id'] = $matches[1];
-        require 'users/delete.php';
-        break;
-
-    default:
-        http_response_code(404);
-        echo json_encode(["message" => "Route not found"]);
-        break;
+// Routeur
+if ($method === 'POST' && preg_match('#^/users/?$#', $requestUri)) {
+    require 'users/create.php';
+} elseif ($method === 'GET' && preg_match('#^/users/?$#', $requestUri)) {
+    require 'users/read.php';
+} elseif ($method === 'GET' && preg_match('#^/users/(\d+)$#', $requestUri, $matches)) {
+    $_GET['id'] = $matches[1];
+    require 'users/read_one.php';
+} elseif ($method === 'PUT' && preg_match('#^/users/(\d+)$#', $requestUri, $matches)) {
+    $_GET['id'] = $matches[1];
+    require 'users/update.php';
+} elseif ($method === 'DELETE' && preg_match('#^/users/(\d+)$#', $requestUri, $matches)) {
+    $_GET['id'] = $matches[1];
+    require 'users/delete.php';
+} else {
+    http_response_code(404);
+    echo json_encode(["message" => "Route not found", "uri" => $requestUri, "method" => $method]);
 }
